705fd79bb26761581ea4f2c9e05344fdd3945c02
nsxzhou <1790146932@qq.com>
2025-02-11 15:10:51 +0800

    25-2-11

--- src/api/system.ts
+++ src/api/system.ts
@@ -9,3 +9,14 @@ export interface captchaType {
 export function GenCaptcha(): Promise<baseResponse<captchaType>> {
   return useAxios.get("/api/system/captcha");
 }
+
+export interface refreshTokenType {
+  token: string;
+  user_id: string;
+}
+
+export function refreshToken(
+  req: refreshTokenType
+): Promise<baseResponse<string>> {
+  return useAxios.post("/api/system/refreshToken", req);
+}
