d594bdcb71cac709187503c620f8ac51500ceb51
nsxzhou <1790146932@qq.com>
2025-06-07 10:09:22 +0800

    移除ImageFilters组件的导出，简化图片管理页面的组件结构，提升代码可读性。

--- src/pages/images/components/index.ts
+++ src/pages/images/components/index.ts
@@ -1,5 +1,4 @@
 export { default as ImageDetailModal } from './ImageDetailModal';
-export { default as ImageFilters } from './ImageFilters';
 export { default as ImageGrid } from './ImageGrid';
 export { default as ImageStats } from './ImageStats';
 export { default as ImageUploadModal } from './ImageUploadModal';
